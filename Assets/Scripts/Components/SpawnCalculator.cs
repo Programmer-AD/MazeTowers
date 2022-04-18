5938b82ca28c5621ce45113b61b5bbb733b8f907
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-04-18 05:01:03 +0300

    Remove maze difficulty from spawn calculation

--- Assets/Scripts/Components/SpawnCalculator.cs
+++ Assets/Scripts/Components/SpawnCalculator.cs
@@ -6,9 +6,8 @@ public static class SpawnCalculator
 {
     public static IEnumerable<SpawnDescription> GetSpawns(int roundNum, MazeTilemapAdapter mazeTilemap)
     {
-        var mazeDifficulty = GetMazeDifficulty(mazeTilemap);
-        var roundDifficulty = (roundNum - 1) / 10f;
-        var difficulty = roundDifficulty / mazeDifficulty;
+        var roundDifficulty = (roundNum - 1) / 5f;
+        var difficulty = roundDifficulty;
 
         var baseSpawnCount = (mazeTilemap.Width + mazeTilemap.Height) / 4f;
         var spawnCount = (int)MathF.Round((1 + difficulty / 100) * baseSpawnCount);
@@ -23,20 +22,6 @@ public static class SpawnCalculator
             new(baseLevel + 1, nextLevelCount)
         };
 
-        return result;
-    }
-
-    private static float GetMazeDifficulty(MazeTilemapAdapter mazeTilemap)
-    {
-        var mazeWidth = mazeTilemap.Width - 2;
-        var mazeHeight = mazeTilemap.Height - 2;
-
-        var minLength = mazeWidth + mazeHeight - 1;
-        var maxLength = (mazeWidth * mazeHeight) / 2f;
-        var averageLength = (maxLength + minLength) / 2f;
-
-        var pathLength = mazeTilemap.Path.Count();
-        var result = averageLength / pathLength;
         return result;
     }
 }
